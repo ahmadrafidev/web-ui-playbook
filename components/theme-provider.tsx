5b0db66ef32ff74c9e4008130551f9180f17907d
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2025-06-16 20:37:55 +0700

    fix unused and types error

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,11 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps as NextThemesProps } from "next-themes"
 
 interface ThemeProviderProps {
   children: React.ReactNode
-  attribute?: string
+  attribute?: NextThemesProps['attribute']
   defaultTheme?: string
   enableSystem?: boolean
   disableTransitionOnChange?: boolean
