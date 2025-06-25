3d2c428b8918ee610d4fc7696cc201fc5187c5ce
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2025-06-26 00:24:44 +0700

    add animation in hover section

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -2,10 +2,10 @@ export function HeroSection() {
   return (
     <section className="mt-2 md:mt-4 mb-8 md:mb-10 px-4">
       <div className="container mx-auto text-center">
-        <h2 className="text-xl sm:text-2xl md:text-4xl font-bold text-foreground mb-2">
+        <h2 className="text-xl sm:text-2xl md:text-4xl font-bold text-foreground mb-2 animate-in fade-in slide-in-from-top-8 duration-700 delay-150">
           The documented web interfaces patterns
         </h2>
-        <p className="text-base sm:text-lg md:text-xl text-muted-foreground mb-6 sm:mb-8 max-w-3xl mx-auto text-wrap px-4 sm:px-0">
+        <p className="text-base sm:text-lg md:text-xl text-muted-foreground mb-6 sm:mb-8 max-w-3xl mx-auto text-wrap px-4 sm:px-0 animate-in fade-in slide-in-from-bottom-6 duration-700 delay-300">
           Essential patterns for building scalable and accessible web interfaces.
         </p>
       </div>
