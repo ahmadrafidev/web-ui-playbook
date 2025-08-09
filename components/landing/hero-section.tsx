cded0f26e904b5d59cc7f81bf4627d2d96a4fa02
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2025-08-09 19:18:44 +0700

    fix animation position issue in hero section

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -5,10 +5,10 @@ export function HeroSection() {
   return (
     <section className="mt-4 sm:mt-8 md:mt-12 mb-4 md:mb-6 px-4">
       <div className="container mx-auto text-center relative overflow-hidden">
-        <h2 className="max-w-4xl text-2xl sm:text-3xl md:text-5xl tracking-tight text-balance font-semibold text-foreground mb-2 animate-in fade-in slide-in-from-top-24 duration-[700ms] ease-[cubic-bezier(0.25,0.46,0.45,0.94);] transform-gpu motion-reduce:animate-none delay-150">
+        <h2 className="max-w-4xl text-2xl sm:text-3xl md:text-5xl tracking-tight text-balance font-semibold text-foreground mb-2 hero-appear anim-delay-100">
           {heading}
         </h2>
-        <p className="mx-auto max-w-3xl text-wrap text-sm sm:text-base md:text-lg text-foreground animate-in fade-in slide-in-from-top-24 duration-[700ms] ease-[cubic-bezier(0.25,0.46,0.45,0.94);] transform-gpu motion-reduce:animate-none delay-150">
+        <p className="mx-auto max-w-3xl text-wrap text-sm sm:text-base md:text-lg text-foreground hero-appear anim-delay-100">
           {subheading}
         </p>
       </div>
