8f39414033a30d4a2866f0473ea9fc2042c2b97f
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2025-12-23 15:54:19 +0700

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,12 +7,12 @@ export function Footer() {
         <p className="text-muted-foreground text-xs md:text-sm font-normal">
           Built by{" "}
           <Link 
-            href="https://x.com/arayyye" 
+            href="https://x.com/rafiiwirana" 
             target="_blank"
             rel="noopener noreferrer"
             className="text-primary hover:text-primary/80 transition-colors hover:underline font-medium"
           >
-            arayyye.
+            Rafi.
           </Link>
         </p>
         <p className="text-muted-foreground text-xs md:text-sm font-normal">
